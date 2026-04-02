5b5900d699cf7c19606f41124ef148b09fecea47
SandyNoqrashy <sandynoqrashy18@gmail.com>
2026-04-02 03:32:52 +0200

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-screen bg-slate-500 text-white text-center py-4 fixed bottom-0 ">
+    <div className="w-screen bg-slate-500 text-white text-center py-4 fixed bottom-0 mt-6">
       © 2026 Sandy. All rights reserved.
     </div>
   );
